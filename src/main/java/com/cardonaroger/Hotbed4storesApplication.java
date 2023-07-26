8dfd5314eb49d9642b4e236be4555f056c9e8a43
rogelocard <rsca@misena.edu.co>
2023-07-25 22:30:00 -0500

    first commit

---
+++ src/main/java/com/cardonaroger/Hotbed4storesApplication.java
@@ -0,0 +1,13 @@
+package com.cardonaroger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hotbed4storesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hotbed4storesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardonaroger/Hotbed4storesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardonaroger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hotbed4storesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
